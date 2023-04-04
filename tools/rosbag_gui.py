c8bad17070ba76de6f837f17e4c5622b68632fa9
Siddhant Wadhwa <siddhant.wadhwa@gmail.com>
2023-04-04 10:57:22 -0400

    improvements to rosbag_gui tool

--- tools/rosbag_gui.py
+++ tools/rosbag_gui.py
@@ -2,6 +2,7 @@ import sys
 import os
 import subprocess
 import json
+from PyQt5.QtCore import QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit, QScrollArea, QCheckBox, QGridLayout, QFrame
 
 class RosbagRecorder(QWidget):
@@ -12,19 +13,21 @@ class RosbagRecorder(QWidget):
         self.topic_checkboxes = {}
         self.initUI()
 
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_timer)
+        self.time_elapsed = QTime(0, 0)
+
     def initUI(self):
         self.setWindowTitle('Rosbag Recorder')
 
         vbox = QVBoxLayout()
 
-        # Create a scroll area to display topic checkboxes
         scroll_area = QScrollArea(self)
         scroll_content = QWidget(scroll_area)
         scroll_layout = QVBoxLayout(scroll_content)
         scroll_area.setWidgetResizable(True)
         scroll_area.setWidget(scroll_content)
 
-        # Fetch and display available topics with checkboxes
         available_topics = self.get_available_topics()
         for topic in available_topics:
             checkbox = QCheckBox(topic, self)
@@ -35,7 +38,6 @@ class RosbagRecorder(QWidget):
         self.output_path_input = QLineEdit(self)
         self.output_path_input.setPlaceholderText("Enter output file name/path")
 
-        # Add QLineEdit widgets for the new metadata keys
         self.gt_range_input = QLineEdit(self)
         self.gt_range_input.setPlaceholderText("Enter gt_range")
         self.gt_viewing_angle_input = QLineEdit(self)
@@ -54,7 +56,15 @@ class RosbagRecorder(QWidget):
         vbox.addWidget(self.gt_viewing_angle_input)
         vbox.addWidget(self.record_button)
 
+        self.timer_label = QLabel("Recording Time: 00:00:00", self)
+        vbox.addWidget(self.timer_label)
+
         self.setLayout(vbox)
+
+    def update_timer(self):
+        self.time_elapsed = self.time_elapsed.addSecs(1)
+        self.timer_label.setText(f"Recording Time: {self.time_elapsed.toString()}")
+
 
     def toggle_recording(self):
         if self.process is None:
@@ -63,6 +73,10 @@ class RosbagRecorder(QWidget):
             self.stop_recording()
 
     def start_recording(self):
+        self.time_elapsed.setHMS(0, 0, 0)
+        self.timer_label.setText("Recording Time: 00:00:00")
+        self.timer.start(1000)  # Update the timer every 1 second (1000 ms)
+
         selected_topics = [topic for topic, checkbox in self.topic_checkboxes.items() if checkbox.isChecked()]
 
         if not selected_topics:
@@ -77,7 +91,6 @@ class RosbagRecorder(QWidget):
         cmd = f"ros2 bag record -o {output_path} {topics}"
         self.process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        # Save metadata to a JSON file
         metadata = {
             'topics': selected_topics,
             'output_file': f"{output_path}.db3",
@@ -96,9 +109,20 @@ class RosbagRecorder(QWidget):
             self.process.terminate()
             self.process.wait()
             self.process = None
+            self.timer.stop()
 
         self.record_button.setText("Start Recording")
 
     def get_available_topics(self):
         cmd = "ros2 topic list"
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        topics = stdout.decode('utf-8').split('\n')
+        topics = [topic.strip() for topic in topics if topic.strip()]
+        return topics
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    recorder = RosbagRecorder()
+    recorder.show()
+    sys.exit(app.exec_())
